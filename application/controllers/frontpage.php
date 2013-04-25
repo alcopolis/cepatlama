df6a9b0b03b2f25109c51d2f67629c46c021e890
alcopolis <myseconddigitalmail@yahoo.com>
2013-04-25 14:45:09 +0700

    Setup Frontend and Backend Controller Files

--- application/controllers/frontpage.php
+++ application/controllers/frontpage.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Frontpage extends CI_Controller {
+class Frontpage extends Frontend_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -19,6 +19,7 @@ class Frontpage extends CI_Controller {
 	 */
 	public function index()
 	{
+		//var_dump($this->data);
 		$this->load->view('welcome_message');
 	}
 }
