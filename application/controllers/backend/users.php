31f45e5e595eb2e788095f93deb1e58652216b48
alcopolis <myseconddigitalmail@yahoo.com>
2013-04-29 17:12:41 +0700

    Basic CRUD function added

--- application/controllers/backend/users.php
+++ application/controllers/backend/users.php
@@ -10,4 +10,21 @@ class Users extends Backend_Controller{
 		$users = $this->users_m->get();
 		var_dump($users);
 	}
+	
+	public function save($data, $id){
+		$data = array(
+			//'email' => 'myseconddigitalmail@yahoo.com',
+			//'password' => '',
+			'name' => 'Adriant R',
+			'role' => '1'
+		);
+		
+		$id = $this->users_m->save($data, 1);
+		var_dump($id);
+	}
+	
+	public function delete($id){
+		$this->users_m->delete($id);
+		echo 'record deleted';
+	}
 }
